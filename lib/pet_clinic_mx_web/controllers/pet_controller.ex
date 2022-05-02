da7631580df74d8bf16b8be2c7ed7bfd28f5406c
Erick Barcenas <barcenas.dev@gmail.com>
2022-05-01 21:02:06 -0500

    Adding functionality to see pet detail, only applies to phoenix

--- lib/pet_clinic_mx_web/controllers/pet_controller.ex
+++ lib/pet_clinic_mx_web/controllers/pet_controller.ex
@@ -36,8 +36,8 @@ defmodule PetClinicMxWeb.PetController do
 
   def show(conn, %{"id" => id}) do
     pet = PetService.get_pet!(id)
-    owner = OwnerService.get_owner!(1) # pet.owner_id
-    expert = HealthExpertService.get_health_expert!(1) # pet.preferred_expert_id
+    owner = OwnerService.get_owner!(pet.owner_id) # 
+    expert = HealthExpertService.get_health_expert!(pet.preferred_expert_id) # 
     render(conn, "show.html", pet: pet, owner: owner, expert: expert)
   end
 
